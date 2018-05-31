53d82baa4502f1e410c78a9326889555b8cefd29
T-rav <tmfrisinger@gmail.com>
2018-05-31 20:24:37 +0200

    Updated to log path and arguments when an exception happens

--- source/Sync.Process.Runner/SynchronousAction.cs
+++ source/Sync.Process.Runner/SynchronousAction.cs
@@ -1,4 +1,5 @@
-﻿using TddBuddy.CleanArchitecture.Domain.Messages;
+﻿using System;
+using TddBuddy.CleanArchitecture.Domain.Messages;
 using TddBuddy.CleanArchitecture.Domain.Output;
 using TddBuddy.Synchronous.Process.Runner.PipeLineTask;
 
@@ -23,30 +24,37 @@ namespace TddBuddy.Synchronous.Process.Runner
         public void Execute(string input, IRespondWithSuccessOrError<string, ErrorOutputMessage> presenter)
         {
             var processStartInfo = _processPipeLineTask.CommandToExecute();
-
-            using (var process = _processFactory.CreateProcess(processStartInfo))
+            
+            try
             {
-                process.Start();
+                using (var process = _processFactory.CreateProcess(processStartInfo))
+                {
+                    process.Start();
 
-                var readerTask = process.ReadStdOutToEndAsync();
-                var errorTask = process.ReadStdErrToEndAsync();
+                    var readerTask = process.ReadStdOutToEndAsync();
+                    var errorTask = process.ReadStdErrToEndAsync();
 
                 process.WriteToStdInput(input);
 
-                process.WaitForExit();
+                    process.WaitForExit();
 
-                var error = errorTask.Result;
-                if (HasError(error))
-                {
-                    var errorOutput = new ErrorOutputMessage();
-                    var trimedArugments = processStartInfo.Arguments.Substring(3);
-                    errorOutput.AddError($"Failed to execute {trimedArugments}");
-                    errorOutput.AddError(error);
-                    presenter.Respond(errorOutput);
-                    return;
+                    var error = errorTask.Result;
+                    if (HasError(error))
+                    {
+                        var errorOutput = new ErrorOutputMessage();
+                        var trimedArugments = processStartInfo.Arguments.Substring(3);
+                        errorOutput.AddError($"Failed to execute {trimedArugments}");
+                        errorOutput.AddError(error);
+                        presenter.Respond(errorOutput);
+                        return;
+                    }
+
+                    presenter.Respond(readerTask.Result);
                 }
-
-                presenter.Respond(readerTask.Result);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to execute [{processStartInfo.FileName}] with [{processStartInfo.Arguments}]",e);
             }
         }
 
