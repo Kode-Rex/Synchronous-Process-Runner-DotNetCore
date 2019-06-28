78d086e1dce4f60813cf8d386dea37c735f140b6
T-rav <t@stoneagetechnologies.com>
2019-06-28 13:08:08 -0700

    feat: added BashPipeLineTask

---
+++ source/Sync.Process.Runner/PipeLineTask/BashPipeLineTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StoneAge.Synchronous.Process.Runner.PipeLineTask
+{
+    public abstract class BashPipeLineTask : ProcessPipeLineTask
+    {
+        private readonly string _arguments;
+        private readonly string _application;
+
+        protected BashPipeLineTask(string application, string arguments)
+        {
+            if (string.IsNullOrWhiteSpace(application)) throw new ArgumentException(nameof(application));
+            _application = application;
+            _arguments = arguments;
+        }
+
+        public override ProcessStartInfo CommandToExecute()
+        {
+            var fileName = "/bin/bash";
+            var arguments = $"{_application} {_arguments}";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                throw new WrongOsException("This task will not run on Windows!");
+            }
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                StandardErrorEncoding = Encoding.UTF8,
+                StandardOutputEncoding = Encoding.UTF8
+            };
+
+            return processStartInfo;
+        }
+    }
+
+    public class WrongOsException : Exception
+    {
+        public WrongOsException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ source/Sync.Process.Runner/PipeLineTask/NodePipeLineTaskv2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace StoneAge.Synchronous.Process.Runner.PipeLineTask
+{
+    public abstract class NodePipeLineTaskv2 : ProcessPipeLineTask
+    {
+        private readonly string _arguments;
+        private readonly string _applicationPath;
+
+        protected NodePipeLineTaskv2(string applicationPath, object arguments)
+        {
+            if (string.IsNullOrWhiteSpace(applicationPath)) throw new ArgumentException(nameof(applicationPath));
+            _applicationPath = applicationPath;
+            _arguments = JsonConvert.SerializeObject(arguments);
+        }
+
+        public override ProcessStartInfo CommandToExecute()
+        {
+            var fileName = "node";
+            var arguments = $"\"{_applicationPath}\"";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                fileName = "cmd.exe";
+                arguments = $"/C node \"{_applicationPath}\"";
+            }
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                StandardErrorEncoding = Encoding.UTF8,
+                StandardOutputEncoding = Encoding.UTF8
+            };
+
+            return processStartInfo;
+        }
+    }
+}
